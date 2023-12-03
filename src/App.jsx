c4edb83062146bbe1208f814522937a2ca6b1520
Abhi <abhilasha.reva61@gmail.com>
2023-12-03 17:49:20 +0100

    firsct commit, created navbar and footer

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Footer from './components/Layout/Footer';
+import Navbar from './components/Layout/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar />
+
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+  <aside className="items-center  grid-flow-col">
+    <p>Copyright © 2023 - All right reserved By Abhilasha Revannavar</p>
+  </aside> 
+</footer>
+  )
+}
+
+export default Footer
